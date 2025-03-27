52d4988037f62a8b5f520ece48247f437bab7986
santhosh <ksdsanthosh130@gmail.com>
2025-03-27 10:38:55 +0530

    Db connected

---
+++ back-end/config.js
@@ -0,0 +1,1 @@
+export const mongodb = "mongodb+srv://vishaalsaravanakumar:Bye5L4W3olLKilLk@hackathon.6bhlwxe.mongodb.net/?retryWrites=true&w=majority&appName=Hackathon"
--- back-end/server.js
+++ back-end/server.js
@@ -1,1 +1,19 @@
-console.log("start panalam");
+import express from "express"
+import {  mongodb} from "./config.js"
+import mongoose  from "mongoose"
+const app = express()
+
+
+mongoose
+    .connect(mongodb)
+    .then(()=>{
+        console.log("database connected")})
+        .catch((err)=>{
+            console.log(err)
+        })
+app.get('/', (req,res) =>{
+    res.send("Hello world");
+})
+app.listen(3000, () => {
+    console.log("server listening in  3000")
+})
